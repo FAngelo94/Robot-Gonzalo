99a19cb970aeb601c0f4914ba87f67b8cf0ea154
FAngelo94 <a.falci@live.it>
2017-04-20 14:55:36 +0200

    DFPlayer
    Code example for DFPlayer

---
+++ DFPlayer.ino
@@ -0,0 +1,49 @@
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(12, 13); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+int song;
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(15);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
+  song=1;
+}
+
+void loop()
+{
+  static unsigned long timer = millis();
+  
+  if (millis() - timer > 3000) {
+    timer = millis();
+    myDFPlayer.play(song);  //Play the first mp3
+    if(song==1)
+      song=2;
+    else
+      song=1;
+  }
+  
+  /*if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }*/
+}
+
