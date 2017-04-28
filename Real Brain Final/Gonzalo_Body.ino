9c820acb56e9670f19c495e03b58127bc28f67de
FAngelo94 <a.falci@live.it>
2017-04-28 10:00:43 +0200

    Add files via upload
    backup before the last day

---
+++ Real Brain Final/Gonzalo_Body.ino
@@ -0,0 +1,128 @@
+/*
+ * Servo Gonzalo
+ * This arduino manage the sonar and movements of body
+ */
+#include <Servo.h>
+#include <NewPing.h>
+
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+#define TRIGGER_PIN1  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN1     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+#define TRIGGER_PIN2  10  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN2    9  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+#define TRIGGER_PIN3  8  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN3    7  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+//Right
+NewPing right(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//Center
+NewPing center(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//Left
+NewPing left(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//distance of every sonar
+int distR, distL, distC;
+double sonarTime;
+//Servo for the rotation and its position
+#define BODY_PIN 6
+Servo bodyMotor;
+
+//Variable to manage Hello
+long helloTime;
+
+void setup() {
+  //BODY
+  //Serial.begin(9600);
+  bodyMotor.attach(BODY_PIN);
+  bodyMotor.write(90);
+  delay(500);
+  bodyMotor.detach();
+  distR=right.ping_cm(); 
+  delay(30);
+  distC=center.ping_cm();
+  delay(30); 
+  distL=left.ping_cm(); 
+  if(distL==0)
+    distL=400;
+  if(distC==0)
+    distC=400;
+  if(distR==0)
+    distR=400;
+  //Set up hello
+  helloTime=0;
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  delay(1000);
+}
+
+void loop() {
+  checkSonar();
+  if(millis()-helloTime>=5000 && (distC>=50 && distC<=350))
+  {
+    //Serial.println("CIAO");
+    sayHello();
+    helloTime=millis();
+  }
+  delay(100);
+}
+
+//MANAGE SONAR AND ROTATION OF ROBOT
+void checkSonar()
+{
+  distR=right.ping_cm();
+  delay(30);
+  distC=center.ping_cm();
+  delay(30);
+  distL=left.ping_cm();
+  delay(30);
+  if(distL==0)
+    distL=400;
+  if(distC==0)
+    distC=400;
+  if(distR==0)
+    distR=400; 
+  bodyMotor.attach(BODY_PIN);
+  if(distR+10<distC && distR+10<distL){
+    for(int i=0;i<45;i++)
+    {
+      if(bodyMotor.read()>30)
+        bodyMotor.write(bodyMotor.read()-1);
+      else 
+        break;
+      delay(20);
+    }
+    
+  }
+  if(distL+10<distC && distL+10<distR){
+    for(int i=0;i<45;i++)
+    {
+      if(bodyMotor.read()<130)
+        bodyMotor.write(bodyMotor.read()+1);
+      else
+        break;
+      delay(20);
+    }
+  }
+  bodyMotor.detach();
+  /*Serial.print("Ping1: ");
+  Serial.print(distR ); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  
+  Serial.print("Ping2: ");
+  Serial.print(distC ); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  
+  Serial.print("Ping3: ");
+  Serial.println(distL ); // Send ping, get distance in cm and print result (0 = outside set distance range)*/
+}
+
+void sayHello()
+{
+  /*
+   * Send to the secondary arduino the command to say hello
+   */
+  digitalWrite(2,HIGH);
+  delay(300);
+  digitalWrite(2,LOW);
+}
+
---
+++ Real Brain Final/Gonzalo_Secondary.ino
@@ -0,0 +1,313 @@
+/*
+ * Angelo Falci 
+ * Emanuele Schiavon
+ * Valentina Favaro
+ * Julian Gallego
+ * Soheil Ghanbari
+ */
+#include <Servo.h>
+#include <DFRobotDFPlayerMini.h>7
+#include <SoftwareSerial.h>
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+//HAND WITH FOOD
+#define FOOD_PIN 7
+#define PIN_SENSOR_1 A0
+#define PIN_SENSOR_2 A1
+#define PIN_SENSOR_3 A2
+Servo foodHand;
+int positionFoodHand;
+long foodTime;
+boolean foodMoveFast;
+int oldValue[3];
+int newValue[3];
+int offerLed=4;
+
+//HELLO HAND
+#define HELLO_PIN 6
+Servo helloHand;
+int positionHelloHand;
+long helloTime;
+long helloInterval;
+int helloDirection;
+
+//eyes
+#define GREEN 9
+#define BLU 10
+#define RED 11
+
+//speak
+SoftwareSerial mySoftwareSerial(12, 13); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+//face
+#define face1 5
+#define face2 4
+Servo faceS1, faceS2;
+long mustacheTime;
+long mustacheDirection;
+long mustacheInterval;
+int mustachePosition;
+
+void setup() {
+  //HAND WITH FOOD
+  positionFoodHand=180;
+  foodHand.attach(FOOD_PIN);
+  foodHand.write(positionFoodHand); 
+  foodMoveFast=false;
+  foodTime=0;
+  oldValue[0]=analogRead(PIN_SENSOR_1);
+  oldValue[1]=analogRead(PIN_SENSOR_2);
+  oldValue[2]=analogRead(PIN_SENSOR_3);
+  pinMode(offerLed, OUTPUT);
+  digitalWrite(offerLed, LOW);
+  //HELLO HAND
+  positionHelloHand=90;
+  helloHand.attach(HELLO_PIN);
+  helloHand.write(positionHelloHand);
+  helloTime=0;
+  helloInterval=0;
+  helloDirection=0;
+  //eye setup
+  pinMode(GREEN, OUTPUT);  
+  pinMode(BLU, OUTPUT);  
+  pinMode(RED, OUTPUT);
+  blueEye();
+  //Set up mustache
+  faceS1.attach(face1);
+  faceS2.attach(face2);
+  //faceS1.write();
+  //faceS2.write();
+  mustacheTime=-3000;
+  mustacheDirection=0;
+  mustacheInterval=0;
+  mustachePosition=90;
+  sadMustache();
+  delay(2000);
+  helloHand.detach();
+  foodHand.detach();
+  helloTime=millis();
+  //Body Arduino send in pin 2 the signal when a person is detected in front of it
+  pinMode(2, INPUT);
+  //Set up the DF Player
+  mySoftwareSerial.begin(9600);
+  //Serial.begin(115200);
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    while(true);
+  }
+  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  //Serial.begin(115200);
+}
+
+void loop() {
+  checkDistance();
+  //moveHandFast();
+  sayhi();
+  laughMustache();
+  sadMustache();
+}
+//Check if the signal of "near person" is activated or not
+void checkDistance(){
+   
+    if(digitalRead(2)==HIGH && millis()-helloTime>=3000)
+    {
+      //Serial.println("CIAO");
+        helloTime=millis();
+    }
+}
+
+//MOVE FAST THE HAND IF A PERSON TRY TO KEEP FOOD
+//When someone try to keep food Gonzalo move hand, say "ahahah" and move mustache to laugh for about 2 second,
+//and then his hand return in the original position
+void moveHandFast()
+{
+  //Serial.println(foodMoveFast);
+  if(foodMoveFast==false && millis()-foodTime>=1000)
+  {
+    newValue[0]=analogRead(PIN_SENSOR_1);
+    newValue[1]=analogRead(PIN_SENSOR_2);
+    newValue[2]=analogRead(PIN_SENSOR_3);
+   /* Serial.print("FOTO=");
+    Serial.print(newValue[0]);
+    Serial.print("-");
+    Serial.print(newValue[1]);
+    Serial.print("-");
+    Serial.println(newValue[2]);*/
+    if(-oldValue[0]+newValue[0]>5 || -oldValue[1]+newValue[1]>5 || -oldValue[2]+newValue[2]>5)
+    {//A person is trying to take food
+      foodMoveFast=true;
+    }
+    oldValue[0]=newValue[0];
+    oldValue[1]=newValue[1];
+    oldValue[2]=newValue[2];
+    
+    foodTime=millis();
+  }
+  if(foodMoveFast==true)
+  {
+      redEye();
+      foodHand.attach(FOOD_PIN);
+      for(int i=180;i>120;i=i-5)
+      {
+        foodHand.write(i);
+        delay(2);
+      }
+    speak("fun");
+    mustacheTime=millis();
+      foodMoveFast=false;
+      foodHand.detach();
+      foodTime=millis();
+      oldValue[0]=analogRead(PIN_SENSOR_1);
+      oldValue[1]=analogRead(PIN_SENSOR_2);
+      oldValue[2]=analogRead(PIN_SENSOR_3);
+      helloTime=millis()+3000; //After move hand fast Gonzalo don't say hi for a while
+      blueEye();
+  }
+  if(foodMoveFast==false && foodHand.read()<=120 && millis()-helloTime>-1000 && millis()-helloTime<0)
+  {
+    foodHand.attach(FOOD_PIN);
+      for(int i=120;i<180;i++)
+      {
+        foodHand.write(i);
+        delay(10);
+      }
+      foodHand.detach();
+    normalMustache();
+  }
+}
+
+//Robot say hi for 3 seconds when the body arduino give the signal
+void sayhi()
+{
+  if(millis()-helloTime<3000 && millis()-helloInterval>20)
+  {
+    //Serial.println("sto salutando");
+  if(!helloHand.attached())  
+  {
+    helloHand.attach(HELLO_PIN);
+    normalMustache();
+    speak("hi");
+    greenEye();
+  }
+  if(helloDirection==0)
+    {
+      positionHelloHand=positionHelloHand+2;
+      if(positionHelloHand>=110)
+      {
+        helloDirection=1;
+      }
+    }
+    else
+    {
+      positionHelloHand=positionHelloHand-2;
+      if(positionHelloHand<=70)
+      {
+        helloDirection=0;
+      }
+    } 
+  helloHand.write(positionHelloHand);
+  helloInterval=millis();
+  }
+  if(millis()-helloTime>=3000 && helloHand.attached())
+  {//detach the servo when Gonzalo not say hello
+    helloHand.detach();
+    blueEye();
+  }
+}
+
+void blueEye()
+{
+  digitalWrite(GREEN, LOW);  
+  digitalWrite(BLU, LOW);  
+  digitalWrite(RED, HIGH);
+}
+void redEye()
+{
+  digitalWrite(GREEN, HIGH);  
+  digitalWrite(BLU, LOW);  
+  digitalWrite(RED, LOW);
+}
+void greenEye()
+{
+  digitalWrite(GREEN, LOW);  
+  digitalWrite(BLU, HIGH);  
+  digitalWrite(RED, LOW);
+}
+
+//MANAGE ROBOT SPEAKING
+void speak(String s)
+{
+  if(s=="hi")
+  {
+    myDFPlayer.play(2);
+  }
+  if(s=="fun")
+  {
+    myDFPlayer.play(1);  
+  }
+}
+
+//MANAGE EYEBROW AND MUSTACHE
+void normalMustache()
+{
+
+  faceS1.attach(face1);
+  faceS2.attach(face2);
+
+  //faceS1.write();
+  //faceS2.write();
+  delay(50);
+  faceS1.detach();
+  faceS2.detach(); 
+}
+//default position when there isn't anyone
+//Gonzalo become sad when it doesn't interact with anyone for a while
+void sadMustache()
+{
+  if(millis()-helloTime>10000 && millis()-foodTime>1000)
+  {
+    faceS1.attach(face1);
+    faceS2.attach(face2);
+
+    //faceS1.write();
+    //faceS2.write();
+    delay(50);
+    faceS1.detach();
+    faceS2.detach(); 
+  }
+}
+void laughMustache()
+{
+  if(millis()-mustacheTime<2000 && millis()-mustacheInterval>15)
+  {
+    if(!faceS1.attached())
+      faceS1.attach(face1);
+    if(!faceS2.attached())
+      faceS2.attach(face2);
+    if(mustacheDirection==0)
+    {
+      mustachePosition=mustachePosition+2;
+      if(mustachePosition>100)
+        mustacheDirection=1;
+    }
+    else
+    {
+      mustachePosition=mustachePosition-2;
+      if(mustachePosition<80)
+        mustacheDirection=0;
+    }
+    //faceS1.write();
+    //faceS2.write();
+    mustacheInterval=millis();
+  }
+  if(millis()-mustacheTime>2000)
+  {
+    if(faceS1.attached())
+      faceS1.detach();
+    if(faceS2.attached())
+      faceS2.detach();
+  }
+}
